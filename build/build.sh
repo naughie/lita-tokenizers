fd23ee0a1efa5268cb734a4733d653ce9ff191b2
naughie <masaton@naughie.com>
2026-05-22 00:33:05 +0900

    Update GitHub Actions

--- build/build.sh
+++ build/build.sh
@@ -2,11 +2,20 @@
 
 set -e
 
-__rand_id="$(od -vAn -N4 -tx1 </dev/urandom | tr -d ' \n')"
-__image_name="lita-tokenizers-cli"
-__container_name="lita-tokenizers-cli-bincopy-${__rand_id}"
+__target_triple="$1"
 
-docker build -t "${__image_name}" -f ./build/Dockerfile .
+__rand_id="$(od -vAn -N4 -tx1 </dev/urandom | tr -d ' \n')"
+__image_name="lita-tokenizers-cli-${__target_triple}"
+__container_name="lita-tokenizers-cli-${__target_triple}-${__rand_id}"
+
+__dist_dir="lita-tokenizers-cli-${__target_triple}"
+__asset="lita-tokenizers-cli-${__target_triple}.tar.gz"
+
+docker build -t "${__image_name}" -f ./build/Dockerfile --build-arg TARGET_TRIPLE="${__target_triple}" .
 docker create --name "${__container_name}" "${__image_name}"
-docker cp "${__container_name}:/lita-tokenizers-cli" ./lita-tokenizers-cli
+
+mkdir -p "${__dist_dir}"
+docker cp "${__container_name}:/lita-tokenizers-cli" "${__dist_dir}/lita-tokenizers-cli"
 docker rm "${__container_name}"
+
+tar czf "${__asset}" "${__dist_dir}"
